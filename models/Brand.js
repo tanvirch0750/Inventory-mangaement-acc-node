49cdeb08a3176c42381cc70207572e1afc47d650
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-23 20:51:07 +0600

    implemented Brand model

---
+++ models/Brand.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const { ObjectId } = mongoose.Schema.Types;
+
+const BrandSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a brand name'],
+      maxLength: 100,
+      unique: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Brand description required'],
+    },
+    email: {
+      type: String,
+      validate: [validator.isEmail, 'Please provide valid email'],
+      lowercase: true,
+    },
+    website: {
+      type: String,
+      validate: [validator.isURL, 'Please provide valid URL'],
+    },
+    location: String,
+    products: [
+      {
+        type: ObjectId,
+        ref: 'Product',
+      },
+    ],
+    suppliers: [
+      {
+        name: String,
+        contactNumber: String,
+        id: {
+          type: ObjectId,
+          ref: 'Supplier',
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ['active, inactive'],
+      default: 'active',
+    },
+  },
+  { timestamps: true }
+);
+
+const Brand = mongoose.model('Brand', BrandSchema);
+
+module.exports = Brand;
