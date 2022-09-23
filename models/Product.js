40251baad84a5f7b4f769fe89d2a344efe74d98b
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-23 22:15:49 +0600

    modified product model

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const { ObjectId } = mongoose.Schema.Types;
 
 const productSchema = mongoose.Schema(
   {
@@ -9,60 +11,56 @@ const productSchema = mongoose.Schema(
       unique: [true, 'This name alreay exist. Name must be unique'],
       minLength: [3, 'Name must be atlest 3 characters'],
       maxLength: [100, 'Name is too large'],
+      lowercase: true,
     },
     description: {
       type: String,
       required: [true, 'Product description required'],
     },
-    price: {
-      type: Number,
-      required: true,
-      min: [0, 'Price can not be negetive'],
-    },
     unit: {
       type: String,
       required: true,
       enum: {
-        values: ['kg', 'litre', 'pcs'],
-        message: `Unit value can't be {VALUE}, must be kg/litre/pcs`,
+        values: ['kg', 'litre', 'pcs', 'bag'],
+        message: `Unit value can't be {VALUE}, must be kg/litre/pcs/bag`,
       },
     },
-    quantity: {
-      type: Number,
-      required: true,
-      min: [0, 'Quantity can not be negetive'],
-      validate: {
-        validator: (value) => {
-          const isInteger = Number.isInteger(value);
-          if (isInteger) {
-            return true;
-          } else {
-            return false;
-          }
+    imageUrls: [
+      {
+        type: String,
+        required: true,
+        validate: {
+          validator: (value) => {
+            if (!Array.isArray(value)) {
+              return false;
+            }
+            let isValid = true;
+            value.forEach((url) => {
+              if (!validator.isURL(url)) {
+                isValid = false;
+              }
+            });
+            return isValid;
+          },
+          message: 'Please provide valid image urls',
         },
       },
-      message: 'Qunatity must be an Integer',
-    },
-    status: {
+    ],
+    category: {
       type: String,
-      enum: {
-        values: ['in-stock', 'out-of-stock', 'discontinued'],
-        message: `Status value can't be {VALUE}, must be in-stock/out-of-stock/discontinued`,
+      required: true,
+    },
+    brand: {
+      name: {
+        type: String,
+        required: true,
+      },
+      id: {
+        type: ObjectId,
+        ref: 'Brand',
+        required: true,
       },
     },
-    // supplier: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: 'Supplier',
-    // },
-    // categories: [
-    //   {
-    //     name: {
-    //       type: String,
-    //       required: true,
-    //     },
-    //     _id: mongoose.Schema.Types.ObjectId,
-    //   },
-    // ],
   },
   {
     timestamps: true,
